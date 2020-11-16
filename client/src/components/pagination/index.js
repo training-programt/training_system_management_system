1b38ef8a77a867996a045e2b6d130304ea6fd72b
伍涛 <wutao@bkvito.com>
2020-11-16 12:02:57 +0800

    分页封装

--- client/src/components/pagination/index.js
+++ client/src/components/pagination/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { Pagination } from 'antd'
 import './index.less'
 const PaginationComponent = (props) => {
+
   return (
     <Pagination
       total={props.pageparams.total}
       showTotal={total => `共 ${total} 条`}
       hideOnSinglePage
+      size="small"
       pageSize={props.pageparams.pageSize}
       defaultCurrent={props.pageparams.page}
       onChange={(page) => props.handlePage(page)}
