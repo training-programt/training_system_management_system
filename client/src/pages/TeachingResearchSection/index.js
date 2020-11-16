d1d70fa025280211307b2fcd5bb942da0342bacb
伍涛 <wutao@bkvito.com>
2020-11-16 13:58:41 +0800

    教研室页面

--- client/src/pages/TeachingResearchSection/index.js
+++ client/src/pages/TeachingResearchSection/index.js
@@ -1,112 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { Input, Select, Space, Button } from 'antd';
 import PaginationComponent from '../../components/pagination'
 import HeaderComponent from '../../components/header'
 import TableComponent from '../../components/table'
 import './index.less'
 
-import api from '../../apis'
+import api from '../../apis/teachingSection'
 
 const { Option } = Select;
 
 const TeachingResearchSection = () => {
 
-  const onSearch = value => console.log(value);
-
-  const dataSource = [
-    {
-      id: '51038475841701',
-      name: "软件工程教研室",
-      major: '软件工程',
-      teacherCount: 20,
-      fullTeacher: 16
-    },
-    {
-      id: '51038475841702',
-      name: "软件工程教研室",
-      major: '软件工程',
-      teacherCount: 20,
-      fullTeacher: 16
-    },
-    {
-      id: '51038475841703',
-      name: "软件工程教研室",
-      major: '软件工程',
-      teacherCount: 20,
-      fullTeacher: 16
-    },
-    {
-      id: '51038475841704',
-      name: "软件工程教研室",
-      major: '软件工程',
-      teacherCount: 20,
-      fullTeacher: 16
-    },
-    {
-      id: '51038475841705',
-      name: "软件工程教研室",
-      major: '软件工程',
-      teacherCount: 20,
-      fullTeacher: 16
-    },
-    {
-      id: '51038475841706',
-      name: "软件工程教研室",
-      major: '软件工程',
-      teacherCount: 20,
-      fullTeacher: 16
-    },
-    {
-      id: '51038475841707',
-      name: "软件工程教研室",
-      major: '软件工程',
-      teacherCount: 20,
-      fullTeacher: 16
-    },
-    {
-      id: '51038475841708',
-      name: "软件工程教研室",
-      major: '软件工程',
-      teacherCount: 20,
-      fullTeacher: 16
-    },
-    {
-      id: '51038475841709',
-      name: "软件工程教研室",
-      major: '软件工程',
-      teacherCount: 20,
-      fullTeacher: 16
-    },
-    {
-      key: '10',
-      id: '51038475841710',
-      name: "软件工程教研室",
-      major: '软件工程',
-      teacherCount: 20,
-      fullTeacher: 16
-    },
-    {
-      id: '51038475841711',
-      name: "软件工程教研室",
-      major: '软件工程',
-      teacherCount: 20,
-      fullTeacher: 16
-    },
-    {
-      id: '51038475841712',
-      name: "软件工程教研室",
-      major: '软件工程',
-      teacherCount: 20,
-      fullTeacher: 16
-    },
-
-  ];
+  const [page, setPage] = useState(1);
+  const [type, setType] = useState('0');
+  const [query, setQuery] = useState('');
+  const [total, setTotal] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [tableData, setTableData] = useState([]);
 
   const column = [
-    { width: 50, render:(text,record,index)=>`${index + 1 + (tableSetting.page - 1) * tableSetting.rows}`},
+    { width: 50, render: (text, record, index) => `${index + 1 + (tableSetting.page - 1) * tableSetting.rows}` },
     { title: 'id', dataIndex: 'id', key: 'id' },
     { title: '名称', dataIndex: 'name', key: 'name' },
     { title: '专业', dataIndex: 'major', key: 'major' },
+    {
+      title: '类型', dataIndex: 'type', render: text => {
+        return text == 1 ? '专业类' : text == 2 ? '学科类' : '管理类'
+      }
+    },
     { title: '教师人数', dataIndex: 'teacherCount', key: 'teacherCount' },
     { title: '专任教师', dataIndex: 'fullTeacher', key: 'fullTeacher' },
     {
@@ -120,32 +41,46 @@ const TeachingResearchSection = () => {
     },
   ];
 
-  const pageparams = {
-    page: 1,
-    pageSize: 12,
-    total: 100,
-  }
-
   const tableSetting = {
     page: 1,
     rows: 12,
     isIndexColumn: true,
   }
 
-  const handlePage = (v) => {
-    pageparams.page = v;
-    console.log(v);
+  const pageparams = {
+    page: page,
+    pageSize: 12,
+    total: total,
   }
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const params = {
+        page: pageparams.page,
+        rows: tableSetting.rows,
+        type: type,
+        query: query
+      }
+      setLoading(true);
+      const res = await api.getDepartmentList(params);
+      setTableData(res.data);
+      setTotal(res.total);
+      setLoading(false);
+    }
+    
+    fetchData();
+    
+  }, [type, query, page])
 
   return (
     <div className="teach-section">
-
       <HeaderComponent title="教研室管理" />
       <div className="body-wrap">
         <div className="filter-container">
           <div className="filter-type">
             <span>教研室分类：</span>
-            <Select className="select-type" defaultValue='0'>
+            <Select className="select-type" defaultValue={type} onChange={value => setType(value)}>
               <Option value="0">全部</Option>
               <Option value="1">专业类</Option>
               <Option value="2">管理类</Option>
@@ -153,18 +88,16 @@ const TeachingResearchSection = () => {
             </Select>
           </div>
           <div className="search-box">
-            <Input.Search placeholder="请输入教研室名称" onSearch={onSearch} enterButton />
+            <Input.Search placeholder="请输入教研室名称" onSearch={value => setQuery(value)} allowClear enterButton />
           </div>
         </div>
 
         <div className="table-container">
-          <TableComponent data={dataSource} column={column} settings={tableSetting} />
-
+          <TableComponent data={tableData} column={column} settings={tableSetting} loading={loading} />
         </div>
-        <PaginationComponent pageparams={pageparams} handlePage={handlePage} />
+        <PaginationComponent pageparams={pageparams} handlePage={v => setPage(v)} />
       </div>
     </div>
   )
 }
-
 export default TeachingResearchSection
