e97be6665f131a13ac0cdbb15d5cc8d9ce25caae
GouYuQing <3319948815@qq.com>
2021-01-12 22:09:45 +0800

    专业表展示

--- server/app/router/major.js
+++ server/app/router/major.js
@@ -1,10 +1,5 @@
 module.exports = app => {
     // 专业
     const { router, controller } = app;
-    router.get('/getMajor', controller.menu.getMajor);
-    // router.post('/allMenu', controller.menu.getAllMenu);
-    // router.post('/menuData', controller.menu.getMenuByRole);
-    // router.post('/addMenu', controller.menu.addMenu);
-    // router.post('/delMenu', controller.menu.delMenu);
-    // router.post('/updataMenu', controller.menu.updataMenu);
+    router.get('/getMajor', controller.major.getMajor);
 };
