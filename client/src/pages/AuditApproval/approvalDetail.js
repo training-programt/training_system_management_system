b420f64c21ea7a6db5968b0e8dd89e20545aea78
shouldhavedone <wt15182161727@163.com0>
2021-03-30 14:25:07 +0800

    feat：【审核审批管理】详情页面搭建

--- client/src/pages/AuditApproval/approvalDetail.js
+++ client/src/pages/AuditApproval/approvalDetail.js
@@ -1,14 +1,199 @@
-import React from 'react'
-import { Button } from 'antd'
+import React, { useMemo, useState } from 'react'
+import { Form, Input, Select, Radio, Button, InputNumber, Space } from 'antd';
 import { Link } from 'react-router-dom'
-import { RollbackOutlined } from '@ant-design/icons';
+import { RollbackOutlined, SaveOutlined } from '@ant-design/icons';
+
+import "./index.less"
 
 
 const ApprovalDetail = () => {
+
+  const [form] = Form.useForm();
+
+  const [courseData, setCourseData] = useState([
+    {
+      _id: '1',
+      name: '高等数学1'
+    },
+    {
+      _id: '2',
+      name: '高等数学2'
+    },
+    {
+      _id: '3',
+      name: '线性代数'
+    },
+  ]);
+
+
+
+  const layout = {
+    labelCol: { span: 5 },
+    wrapperCol: { span: 17 },
+  };
+
   return (
-    <div>
-      添加
-      <Link to="/auditApproval/approval" style={{ color: '#000', marginLeft: '8px' }}><Button icon={<RollbackOutlined />}>返回</Button></Link>
+    <div className="detail-container">
+      <div className="header-wrap">
+        <div className="header-left">
+          新增审批
+        </div>
+        <div className="header-right">
+          <Space size='small'>
+            <Link to="/auditApproval/approval" style={{ color: '#000', marginLeft: '8px' }}><Button icon={<RollbackOutlined />}>返回</Button></Link>
+            <Button type='primary' icon={<SaveOutlined />}>保存</Button>
+          </Space>
+        </div>
+      </div>
+
+      <div className='body-wrap'>
+
+        <div className='body-left'>
+          <div className='box-wrap'>
+            <div className='title'>一、基本信息</div>
+            <Form
+              {...layout}
+              form={form}
+              name="basic"
+            >
+              <Form.Item
+                label="考核课程"
+                name="course"
+                rules={[
+                  {
+                    required: true,
+                    message: '考核课程不能为空!',
+                  },
+                ]}
+              >
+                <Select
+                  allowClear
+                  placeholder="选择考核课程"
+                >
+                  {
+                    courseData.map(item => <Option key={item._id} value={item._id}>{item.name}</Option>)
+                  }
+                </Select>
+              </Form.Item>
+              <Form.Item
+                label="考核对象"
+                name="object"
+                rules={[
+                  {
+                    required: true,
+                    message: '考核对象不能为空!',
+                  },
+                ]}
+              >
+                <Input allowClear placeholder='请输入考核对象' />
+              </Form.Item>
+              <Form.Item
+                label="考核形式"
+                name="form"
+                rules={[
+                  {
+                    required: true,
+                    message: '考核形式不能为空!',
+                  },
+                ]}
+              >
+                <Radio.Group name="form" defaultValue={1}>
+                  <Radio value={1}>考试</Radio>
+                  <Radio value={2}>考查</Radio>
+                </Radio.Group>
+              </Form.Item>
+              <Form.Item
+                label="考核学生数"
+                name="studentNum"
+                rules={[
+                  {
+                    required: true,
+                    message: '考核学生数不能为空!',
+                  },
+                ]}
+              >
+                <InputNumber
+                  style={{
+                    width: 200,
+                  }}
+                  min={1} />
+              </Form.Item>
+              <Form.Item
+                label="预计及格率"
+                name="estimatePassRate"
+                rules={[
+                  {
+                    required: true,
+                    message: '不能为空!',
+                  },
+                ]}
+              >
+                <InputNumber
+                  style={{
+                    width: 200,
+                  }}
+                  defaultValue={100}
+                  min={0}
+                  max={100}
+                  formatter={value => `${value}%`}
+                  parser={value => value.replace('%', '')}
+                />
+              </Form.Item>
+              <Form.Item
+                label="预计平均分"
+                name="estimateAverage"
+                rules={[
+                  {
+                    required: true,
+                    message: '不能为空!',
+                  },
+                ]}
+              >
+                <InputNumber
+                  style={{
+                    width: 200,
+                  }}
+                  defaultValue="1"
+                  min="0"
+                  max="100"
+                  step="0.0001"
+                  stringMode
+                />
+              </Form.Item>
+
+
+            </Form>
+
+          </div>
+
+          <div className='box-wrap'>
+            <div className='title'>三、审批意见</div>
+            <Form
+              {...layout}
+              form={form}
+              name="basic"
+            >
+              <Form.Item
+                label="审批意见"
+                name="opinion"
+              >
+                <Input.TextArea
+                  placeholder="教研室审批意见"
+                  autoSize={{ minRows: 6, maxRows: 8 }}
+                  disabled
+                />
+              </Form.Item>
+            </Form>
+          </div>
+
+        </div>
+        <div className='body-right'>
+          <div className='box-wrap'>
+            <div className='title'>二、评价标准</div>
+            <div>还不知道怎么布局</div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- client/src/pages/AuditApproval/auditDetail.js
+++ client/src/pages/AuditApproval/auditDetail.js
@@ -1,14 +1,144 @@
-import React from 'react'
-import { Button } from 'antd'
+import React, { useMemo, useState } from 'react'
+import { Form, Input, Select, Radio, Button, InputNumber, Space } from 'antd';
 import { Link } from 'react-router-dom'
-import { RollbackOutlined } from '@ant-design/icons';
+import { RollbackOutlined, SaveOutlined } from '@ant-design/icons';
+import api from '@/apis/auditApproval'
+import "./index.less"
 
 const AuditDetail = () => {
-  return (
-    <div>
-      审核详情
-      <Link to="/auditApproval/audit" style={{ color: '#000', marginLeft: '8px' }}><Button icon={<RollbackOutlined />}>返回</Button></Link>
+  const [form] = Form.useForm();
 
+  const [courseData, setCourseData] = useState([
+    {
+      _id: '1',
+      name: '高等数学1'
+    },
+    {
+      _id: '2',
+      name: '高等数学2'
+    },
+    {
+      _id: '3',
+      name: '线性代数'
+    },
+  ]);
+
+  const getCourseData = async () => {
+    const res = await React.$axios.get(api.getCourseData);
+    if(res && res.isSucceed ) {
+      console.log(res.data)
+    }
+  }
+
+  useMemo(() => {
+    // getCourseData();
+  }, [])
+
+  const layout = {
+    labelCol: { span: 5 },
+    wrapperCol: { span: 17 },
+  };
+
+  return (
+    <div className="detail-container">
+      <div className="header-wrap">
+        <div className="header-left">
+          新增审核
+        </div>
+        <div className="header-right">
+          <Space size='small'>
+            <Link to="/auditApproval/approval" style={{ color: '#000', marginLeft: '8px' }}><Button icon={<RollbackOutlined />}>返回</Button></Link>
+            <Button type='primary' icon={<SaveOutlined />}>保存</Button>
+          </Space>
+        </div>
+      </div>
+
+      <div className='body-wrap'>
+
+        <div className='body-left'>
+          <div className='box-wrap'>
+            <div className='title'>一、基本信息</div>
+            <Form
+              {...layout}
+              form={form}
+              name="basic"
+            >
+              <Form.Item
+                label="课程"
+                name="course"
+                rules={[
+                  {
+                    required: true,
+                    message: '课程不能为空!',
+                  },
+                ]}
+              >
+                <Select
+                  allowClear
+                  placeholder="选择课程"
+                >
+                  {
+                    courseData.map(item => <Option key={item._id} value={item._id}>{item.name}</Option>)
+                  }
+                </Select>
+              </Form.Item>
+              <Form.Item
+                label="是否达成"
+                name="reach"
+                rules={[
+                  {
+                    required: true,
+                    message: '请选择是否达成!',
+                  },
+                ]}
+              >
+                <Radio.Group name="reach" defaultValue={1}>
+                  <Radio value={1}>是</Radio>
+                  <Radio value={2}>否</Radio>
+                </Radio.Group>
+              </Form.Item>
+              <Form.Item
+                label="课程培养情况说明"
+                name="explain"
+                rules={[
+                  {
+                    required: true,
+                    message: '课程培养情况说明不能为空!',
+                  },
+                ]}
+              >
+                <Input.TextArea
+                  placeholder="请输入课程培养情况说明"
+                  autoSize={{ minRows: 4, maxRows: 8 }}
+                />
+              </Form.Item>
+            </Form>
+
+          </div>
+          <div className='box-wrap'>
+            <div className='title'>二、定量达成情况</div>
+            <div>还不知道怎么布局</div>
+          </div>
+
+        </div>
+        <div className='body-right'>
+          <div className='box-wrap'>
+            <div className='title'>三、审核意见</div>
+            <div className='opinion-wrap'>
+              <div className='opinion-type'>教研室意见：</div>
+              <div>我是一段文字我是一段文字我是一段文字我是一段文字我是一段文字我是一段文字</div>
+              <div>Tom</div>
+              <div>2021年3月1日</div>
+            </div>
+            <div className='opinion-wrap'>
+              <div className='opinion-type'>学院意见：</div>
+              <div>我是一段文字我是一段文字我是一段文字我是一段文字我是一段文字我是一段文字</div>
+              <div>Tom</div>
+              <div>2021年3月1日</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
